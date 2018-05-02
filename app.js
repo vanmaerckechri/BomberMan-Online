722fa6c0e2995f150054b75660feded34d64ac92
vanmaerckechri <vanmaerckechri@gmail.com>
2018-05-02 16:45:55 +0200

    problem socket resolu

--- app.js
+++ app.js
@@ -35,109 +35,109 @@ app.use(session(
 	cookie: { secure: false } // true pour l'https.
 }))
 
-// Middlewares Custom.
-
-// Messages d'Alerte.
-app.use(require('./middlewares/alert'));
-
 // ROUTES!
 
 // Route Principale.
 app.get('/', (req, res) =>
 {
 	req.session.playerInfo = {};
-    res.render('pages/connexion');
+		res.render('pages/index',
+	{
+		main: 'connexion'
+	});
 });
+
+function validatePseudo(pseudo)
+{
+	let errorSms;
+	if (pseudo === undefined || pseudo === '')
+	{
+		errorSms = "Vous n'avez pas entré de pseudo!";
+		return [false, errorSms];
+	}
+
+	let reg = /^[a-z0-9]+$/i;
+	let contentFilter = pseudo.match(reg);
+	if (contentFilter === null)
+	{
+		errorSms = "Le pseudo ne peut être composé que de lettres et de chiffres!";
+		return [false, errorSms];
+	}
+
+	if (pseudo.length < 4 || pseudo.length > 16)
+	{
+		errorSms = "Le pseudo doit comporter entre 4 et 16 caractères";
+		return [false, errorSms];
+	}
+	return [true, pseudo];
+}
 
 let lobbies = [];
 let pplByLobby = 4;
+function createLobby(socket)
+{
+	socket.join(socket.id);
+	socket.room = socket.id;
+	// let lobby = [id room, nom du manager, ppl2, ppl3, ...(en fonction de 'pplByLobby'), true pour room ouverte(il reste de la place)]
+	let lobby = [socket.id, socket.name];
+	for (let i = 0, length = pplByLobby - 1; i < length; i++)
+	{
+		lobby.push('');
+	}
+	lobby.push('true');
+	lobbies.push(lobby);
+	socket.broadcast.emit('refreshLobbiesList', lobbies);
+	console.log(lobbies);
+}
+
 io.sockets.on('connection', function(socket)
 {
-	// Enregistrement du Pseudo.
-	app.post('/', (req, res) =>
+
+	socket.on('pullPseudo', function()
 	{
-		let pseudo = ent.encode(req.body.pseudo);
-		if (pseudo === undefined || pseudo === '')
-		{
-			req.alert('error', "Vous n'avez pas entré de pseudo!");
-			res.redirect('/');
-		}
-		else
-		{
-			let reg = /^[a-z0-9]+$/i;
-			let contentFilter = pseudo.match(reg);
-			if (contentFilter === null)
-			{
-				req.alert('error', "Le pseudo ne peut être composé que de lettres et de chiffres!");
-				res.redirect('/');
-			}
-			else
-			{
-				if (pseudo.length < 4 || pseudo.length > 16)
-				{
-					req.alert('error', "Le pseudo doit comporter entre 4 et 16 caractères");
-					res.redirect('/');
-				}
-				else
-				{
-					socket.name = pseudo;
-					req.session.playerInfo['pseudo'] = pseudo;
-					res.render('pages/index',
-					{
-						main: 'menu'
-					});
-				}
-			}
-		}
+		socket.emit('pullPseudo', req.session.playerInfo['pseudo']);
 	});
 
-	// Créer un Lobby.
-	app.get('/create', (req, res) =>
+	socket.on('recordNewPlayerInfo', function(pseudo)
 	{
-		if (req.session.playerInfo === undefined || req.session.playerInfo === '' || req.session.playerInfo['pseudo'] === undefined || req.session.playerInfo['pseudo'] === '')
+			console.log(socket.id);
+
+		pseudoEncode = ent.encode(pseudo);
+		let pseudoValide = validatePseudo(pseudo)
+
+		if (pseudoValide[0] === true)
 		{
-			req.alert('error', "Vous n'avez pas entré de pseudo!");
-			res.redirect('/');
+			socket.name = pseudoValide[1];
 		}
-		else
-		{
-			socket.join(socket.id);
-			socket.room = socket.id;
-			// let lobby = [id room, nom du manager, ppl2, ppl3, ...(en fonction de 'pplByLobby'), true pour room ouverte(il reste de la place)]
-			let lobby = [socket.id, socket.name];
-			for (let i = 0, length = pplByLobby - 1; i < length; i++)
-			{
-				lobby.push('');
-			}
-			lobby.push('true');
-			lobbies.push(lobby);
-			console.log(lobbies);
-			socket.broadcast.emit('refreshLobbiesList', lobbies);
-			res.render('pages/index',
-			{
-				main: 'lobby'
-			});
-		}
+		socket.emit('validatePseudo', pseudoValide);
 	});
-	// Afficher la Page des Lobbies.
-	app.get('/lobbiesList', (req, res) =>
-	{
-		if (req.session.playerInfo === undefined || req.session.playerInfo === '' || req.session.playerInfo['pseudo'] === undefined || req.session.playerInfo['pseudo'] === '')
-		{
-			req.alert('error', "Vous n'avez pas entré de pseudo!");
-			res.redirect('/');
-		}
-		else
-		{
-		    res.render('pages/index',
-		    {
-		    	main: 'lobbiesList'
-		    });
-		}
-	});
+
 	// Refresh la Liste des Lobbies.
-	socket.on('refreshLobbiesList', function (list)
+	socket.on('refreshLobbiesList', function(list)
 	{
 		socket.emit('refreshLobbiesList', lobbies);
 	});
+
+	// Créer un Lobby.
+	socket.on('createLobby', function()
+	{
+		createLobby(socket)
+	});
+});
+
+// Créer un Lobby.
+app.get('/createLobby', (req, res) =>
+{
+	res.render('pages/index',
+	{
+		main: 'lobby'
+	});
+});
+// Afficher la Page des Lobbies.
+app.get('/lobbiesList', (req, res) =>
+{
+    res.render('pages/index',
+    {
+    	main: 'lobbieslist'
+    });
 });
--- public/js/client.js
+++ public/js/client.js
@@ -1,6 +1,6 @@
 let socket = io.connect(window.location.host);
 
-socket.on('refreshLobbiesList', function (list)
+socket.on('refreshLobbiesList', function(list)
 {
 	if (document.querySelector('#lobbiesList'))
 	{
@@ -13,7 +13,54 @@ socket.on('refreshLobbiesList', function (list)
 	}
 });
 
+socket.on('checkSocket', function ()
+{
+	socket.emit('checkSocket');
+});
+
 window.addEventListener('load', function()
 {
 	socket.emit('refreshLobbiesList');
+
+	// Connexion
+	if (document.querySelector('#submitPseudo'))
+	{
+		let main = document.querySelector('#main');
+		let submit = document.querySelector('#submitPseudo');
+		let pseudo = document.querySelector('#pseudo');
+		submit.addEventListener('click', function(e)
+		{
+			e.preventDefault();
+			socket.emit('recordNewPlayerInfo', pseudo.value);
+			socket.on('validatePseudo', function (pseudoValide)
+			{
+				console.log(pseudoValide);
+				if (pseudoValide[0] === true)
+				{
+					let menu = '<h1>Lobby</h1>';
+					menu +=	'<div class="menuMain">';
+					menu +=	'<h2>Menu Principal</h2>';
+					menu += '<a id="createLobby" class="button" href="/createLobby">Créer un Lobby</a>';
+					menu +=	'<a id="loadLobbiesList" class="button" href="/lobbiesList">Rejoindre un Lobby</a>';
+					menu +=	'</div>';
+					main.innerHTML = menu;
+					// Menu Principal.
+					let create = document.querySelector('#createLobby');
+					create.addEventListener('click', function()
+					{
+						socket.emit('createLobby');
+					});
+				}
+				else
+				{
+					let errorSms = document.querySelector('.error');
+									console.log(errorSms);
+
+					errorSms.innerHTML = pseudoValide[1];
+
+				}
+			});
+
+		});
+	}
 });
